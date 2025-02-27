31dcb378a12a8caa57ade3f646f16cd6cd990090
Alexander Scorpion <alexander.scorpion.100@gmail.com>
2025-02-27 10:03:39 +0000

    message box#

--- Development/Life Log/Forms/Dialogs/MessageBoxDialogForm.Designer.cs
+++ Development/Life Log/Forms/Dialogs/MessageBoxDialogForm.Designer.cs
@@ -227,23 +227,6 @@
             // pictureEdit1
             // 
             this.pictureEdit1.Location = new System.Drawing.Point(8, 8);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            this.pictureEdit1.MaximumSize = new System.Drawing.Size(130, 130);
-            this.pictureEdit1.MenuManager = this.ribbon;
-            this.pictureEdit1.MinimumSize = new System.Drawing.Size(130, 130);
-            this.pictureEdit1.Name = "pictureEdit1";
-            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pictureEdit1.Size = new System.Drawing.Size(130, 130);
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             this.pictureEdit1.MaximumSize = new System.Drawing.Size(70, 70);
             this.pictureEdit1.MenuManager = this.ribbon;
             this.pictureEdit1.MinimumSize = new System.Drawing.Size(70, 70);
@@ -252,16 +235,6 @@
             this.pictureEdit1.Properties.ReadOnly = true;
             this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
             this.pictureEdit1.Size = new System.Drawing.Size(70, 70);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             this.pictureEdit1.StyleController = this.layoutControl2;
             this.pictureEdit1.TabIndex = 5;
             // 
