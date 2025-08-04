9fecfea92ec4737e900875d39559fd343b8b0f0f
Alexandre Filipe <alexfilipemb.98@gmail.com>
2025-08-04 23:27:24 +0100

    Update data

--- Development/Base/LifeLog.Base.Models/Properties/AssemblyInfo.cs
+++ Development/Base/LifeLog.Base.Models/Properties/AssemblyInfo.cs
@@ -30,8 +30,13 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: AssemblyVersion("1.0.1.5")]
 [assembly: AssemblyFileVersion("1.0.1.5")]
+=======
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+>>>>>>> Stashed changes
 =======
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
